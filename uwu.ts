4824cb137e5fec7fa6b7e0104d47e855d7a94210
Dave Caruso <dave@davecode.net>
2022-07-24 16:38:31 -0400

    pick up dust off ground

--- uwu.ts
+++
@@ -1,8 +0,0 @@
-import { createSelection } from 'bun-promptx';
-
-createSelection([
-  { text: 'uwu', description: 'The uwu uwu yeah' },
-  { text: 'owo', description: 'The owo whats this owo' },
-  { text: 'uwu', description: 'The uwu again' },
-  { text: 'owo', description: 'The owo again' },
-]);
