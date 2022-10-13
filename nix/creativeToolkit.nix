639fd619a7f77419daf5722ae79f65cb5a7a795e
Dave Caruso <me@paperdave.net>
2022-10-13 01:30:28 -0400

    no longer best friends with nix

--- nix/creativeToolkit.nix
+++
@@ -1,9 +0,0 @@
-with builtins;
-{ pkgs ? import <nixpkgs> { }
-, ctPackages
-, ...
-}:
-pkgs.stdenv.mkDerivation rec {
-  pname = "creative-toolkit";
-  version = (builtins.fromJSON (builtins.readFile ../package.json)).version;
-}
