91a1f2e3e6141624e0039a6e3c617dad0219a0c1
Dave Caruso <me@paperdave.net>
2022-11-08 12:33:47 -0500

    improve cli runner

--- src/cmd-runner/cli.ts
+++ src/cmd-runner/cli.ts
@@ -1,16 +1,48 @@
 #!/usr/bin/env bun
-
 import path from "path";
 import { hint } from "$logger";
 import { Project, resolveProject } from "$project";
-import { injectLogger, Logger } from "@paperdave/logger";
+import { chalk, injectLogger, Logger } from "@paperdave/logger";
 import { pathExists } from "@paperdave/utils";
 import { CommandEvent } from "./types";
+import { readdirSync, readFileSync } from "fs";
+import * as YAML from 'yaml';
+import { TOOLKIT_DATE } from "$constants";
 
 const commandName = process.argv[2];
 
 if (!commandName) {
-  Logger.error("usage: ct <cmd> [...]");
+  Logger.writeLine(chalk.greenBright(`dave caruso's creative toolkit, ${TOOLKIT_DATE}`));
+  Logger.writeLine(chalk.grey("usage: ct <cmd> [...]"));
+  Logger.writeLine('')
+
+  const cmds = readdirSync(path.join(import.meta.dir, '..'))
+    .filter(x => x.startsWith('cmd-') && x !== 'cmd-runner')
+    .map(name => {
+      try {
+        const parsed = YAML.parse(readFileSync(path.join(import.meta.dir, '..', name, 'meta.yaml')).toString());
+        return {
+          sort: 0,
+          ...parsed,
+          name: name.slice(4)
+        };
+      } catch (error) {
+        Logger.warn(`could not find src/${name}/meta.yaml`);
+        return {
+          sort: 0,
+          name: name.slice(4),
+          desc: ''
+        };
+      }
+    })
+    .sort((a, b) => b.sort - a.sort);
+  
+  const padding = Math.max(...cmds.map(x => x.name.length)) + 2;
+
+  for (const cmd of cmds) {
+    const space = ' '.repeat(padding - cmd.name.length);
+    Logger.writeLine(`- ${chalk.green('ct ' + cmd.name)}:${space}${cmd.desc}`)
+  }
   process.exit(1);
 }
 
@@ -31,13 +63,14 @@ if (
 }
 
 if (commandName === "runner") {
-  Logger.error("cannot run the command runner a command.");
+  Logger.error("cannot run the command runner as a command.");
   process.exit(1);
 }
 
 injectLogger();
 try {
   const command = await import(`../cmd-${commandName}/index.ts`);
+  const meta = YAML.parse(readFileSync(path.join(import.meta.dir, `../cmd-${commandName}/meta.yaml`)).toString());
 
   let project!: Project;
   if (command.project === undefined || command.project === true) {
